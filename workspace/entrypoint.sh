256980c5d3d5514eb82203eee5498747120426ad
Zhilskiy, Dmitriy <Dmitriy.Zhilskiy@ocsico.com>
2018-08-30 15:24:32 +0300

    fix mysql default settings

--- workspace/entrypoint.sh
+++ workspace/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 docker_passenger_api_cmd="docker exec -w /var/www/html/ramzy-rides-passenger-api -t laradock_workspace_1"
+docker_worker_cmd="docker exec -w /var/www/html/ramzy-rides-passenger-api -t laradock_php-worker_1"
 
 $docker_passenger_api_cmd composer install
 $docker_passenger_api_cmd php artisan migrate:refresh --seed
+
+$docker_worker_cmd supervisorctl stop all
+$docker_worker_cmd supervisorctl start all
